b4ef9ca92a260fd4588dfdef40d8fa89c4912347
Do-Duy-Hieu <72023188+ntvanank1250@users.noreply.github.com>
2022-06-13 15:48:08 +0700

    Create delete.py

---
+++ delete.py
@@ -0,0 +1,16 @@
+## xóa từng cái một trên target và trên map. lần nào chạy xóa lần đó
+order_id = self.get_map_field_by_src(self.TYPE_ORDER, convert['id'], convert['code'])
+if order_id:
+	self.api('orders/'+to_str(order_id)+'.json',None,'DELETE')
+	self.select_raw("DELETE FROM migration_map WHERE type = 'order' AND id_src = "+to_str(convert['id']))
+  
+ ## xóa hết 1 lần tùy entity mà phần xóa map thay đổi
+
+ if 'product' not in self._notice:
+			self._notice['product'] = True
+			self.clear_target_products()
+			self.delete_obj('migration_map', where={'migration_id': self._migration_id, 'type': 'product'})
+			self.delete_obj('migration_map', where={'migration_id': self._migration_id, 'type': 'product_child'})
+			self.delete_obj('migration_map', where={'migration_id': self._migration_id, 'type': 'attr'})
+			self.delete_obj('migration_map', where={'migration_id': self._migration_id, 'type': 'attr_option'})
+			self.delete_obj('migration_map', where={'migration_id': self._migration_id, 'type': 'attr_value'})
