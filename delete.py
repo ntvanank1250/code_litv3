a8f545200fadd6c6fbc0ab9f1c3d3c588feb8438
Do-Duy-Hieu <72023188+ntvanank1250@users.noreply.github.com>
2022-08-26 08:34:35 +0700

    Update delete.py

--- delete.py
+++ delete.py
@@ -3,14 +3,22 @@ order_id = self.get_map_field_by_src(self.TYPE_ORDER, convert['id'], convert['co
 if order_id:
 	self.api('orders/'+to_str(order_id)+'.json',None,'DELETE')
 	self.select_raw("DELETE FROM migration_map WHERE type = 'order' AND id_src = "+to_str(convert['id']))
+	
+	
+if id_desc:
+	self.api('products/'+to_str(id_desc)+'.json',None,'DELETE')
+	self.select_raw("DELETE FROM migration_map WHERE type = 'product' AND id_src = "+to_str(convert['id']))
+	for child in convert['children']:
+		self.select_raw("DELETE FROM migration_map WHERE type = 'product_child' AND id_src = "+to_str(child['id']))
+
   
  ## xóa hết 1 lần tùy entity mà phần xóa map thay đổi
 
  if 'product' not in self._notice:
-			self._notice['product'] = True
-			self.clear_target_products()
-			self.delete_obj('migration_map', where={'migration_id': self._migration_id, 'type': 'product'})
-			self.delete_obj('migration_map', where={'migration_id': self._migration_id, 'type': 'product_child'})
-			self.delete_obj('migration_map', where={'migration_id': self._migration_id, 'type': 'attr'})
-			self.delete_obj('migration_map', where={'migration_id': self._migration_id, 'type': 'attr_option'})
-			self.delete_obj('migration_map', where={'migration_id': self._migration_id, 'type': 'attr_value'})
+	self._notice['product'] = True
+	self.clear_target_products()
+	self.delete_obj('migration_map', where={'migration_id': self._migration_id, 'type': 'product'})
+	self.delete_obj('migration_map', where={'migration_id': self._migration_id, 'type': 'product_child'})
+	self.delete_obj('migration_map', where={'migration_id': self._migration_id, 'type': 'attr'})
+	self.delete_obj('migration_map', where={'migration_id': self._migration_id, 'type': 'attr_option'})
+	self.delete_obj('migration_map', where={'migration_id': self._migration_id, 'type': 'attr_value'})
